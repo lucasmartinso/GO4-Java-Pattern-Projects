4c874f61684b37ecf22ef5f22435a20fea79534b
lucasmartino <lucasmartinsoliveiraa@gmail.com>
2026-09-10 00:00:18 -0300

    feat: add service factory router

--- src/main/java/factorymethod/ServicoFactory.java
+++ src/main/java/factorymethod/ServicoFactory.java
@@ -1,4 +1,22 @@
 package factorymethod;
 
 public class ServicoFactory {
+
+    public static Servico obterService(String servico) {
+        Class classe = null;
+        Object objeto = null;
+
+        try {
+            classe = Class.forName("factorymethod.Servico" + servico);
+            objeto = classe.newInstance();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Servico Inexistente" + e);
+        }
+
+        if(!(objeto instanceof Servico)) {
+            throw new IllegalArgumentException("Servico Invalido");
+        }
+
+        return (Servico) objeto;
+    }
 }
